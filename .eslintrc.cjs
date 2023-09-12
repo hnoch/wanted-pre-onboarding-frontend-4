a266bc9a6600a8f8578f833ca901594a9ee9fa53
hnoch <hnochx@gmail.com>
2023-09-12 22:05:21 +0900

    ✨ feat: eslint & prettier & husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-var': 'error', // var 금지
+    'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
+    eqeqeq: 'error', // 일치 연산자 사용 필수 === , 약한 비교 (==) 사용 금지!
+    'dot-notation': 'error', // 가능하다면 dot notation 사용
   },
 }
